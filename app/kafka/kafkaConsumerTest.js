951094f13c200ca422ffd8046fa712e2b50f46b2
Andres Munoz Uniandes <af.munoz11@uniandes.edu.co>
2018-05-07 19:59:58 -0500

    Kafka connection POC refactored with the added kafkaClient

--- app/kafka/kafkaConsumerTest.js
+++ app/kafka/kafkaConsumerTest.js
@@ -1,24 +1,5 @@
-const PAYMETN_TOPIC = "lumenconcept.payment";
+var kafkaClient = require("./kafkaClient");
 
-var kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
-    // client = new kafka.Client(),
-    client = new kafka.KafkaClient({kafkaHost: '157.253.238.226:8089'});
-    consumer = new Consumer(client,
-        [{ topic: PAYMETN_TOPIC, offset: 0}],
-        {
-            autoCommit: false
-        }
-    );
-
-consumer.on('message', function (message) {
+kafkaClient.suscribe(function(message) {
     console.log(message);
-});
-
-consumer.on('error', function (err) {
-    console.log('Error:',err);
-});
-
-consumer.on('offsetOutOfRange', function (err) {
-    console.log('offsetOutOfRange:',err);
 });
